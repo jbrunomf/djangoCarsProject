57ed5f414d3d502d7cbb539fed58a0318b4989da
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2024-03-09 22:54:11 -0400

    atualizando

---
+++ cars/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import cars_view
+
+app_name = 'cars'
+urlpatterns = [
+    path('', cars_view, name='index')
+]
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cars.models import Car
+
+
+def cars_view(request):
+    cars = Car.objects.all()
+    return render(request, 'cars.html', {'cars': cars})
+
