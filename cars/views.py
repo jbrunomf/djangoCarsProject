5e81e5c5a69def4fbe67d779010a3ac736d901a2
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2024-03-10 21:17:25 -0400

    cadastro de carros

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from cars.models import Car
+from cars import forms
 
 
 def cars_view(request):
@@ -9,3 +10,15 @@ def cars_view(request):
     if search_string:
         cars = Car.objects.filter(model__contains=search_string)
     return render(request, 'cars.html', {'cars': cars})
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        form = forms.CarForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('cars_list')
+    else:
+        form = forms.CarForm()
+    return render(request, 'new_car.html', {'form': form})
+
