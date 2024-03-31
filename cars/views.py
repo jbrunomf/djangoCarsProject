7882efe3bb3a13dc431a1a5ec8aa535e0118bd86
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2024-03-31 17:41:24 -0400

    utilizando CBV na view de adição de veículo

--- cars/views.py
+++ cars/views.py
@@ -5,16 +5,6 @@ from cars.models import Car
 from cars import forms
 
 
-# def cars_view(request):
-#     if not request.user.is_authenticated:
-#         return redirect('login')
-#
-#     cars = Car.objects.all().order_by('model')
-#     search_string = request.GET.get('search')
-#     if search_string:
-#         cars = Car.objects.filter(model__contains=search_string)
-#     return render(request, 'cars.html', {'cars': cars})
-
 class ListCarView(View):
     def get(self, request):
         if not request.user.is_authenticated:
@@ -26,13 +16,17 @@ class ListCarView(View):
             cars = Car.objects.filter(model__icontains=search_string)
         return render(request, 'cars.html', {'cars': cars})
 
+class NewCarView(View):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect("login")
+        form = forms.CarModelForm()
+        return render(request, 'new_car.html', {'form': form})
 
-def new_car_view(request):
-    if request.method == 'POST':
+    def post(self, request):
         form = forms.CarModelForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('cars_list')
-    else:
-        form = forms.CarModelForm()
-    return render(request, 'new_car.html', {'form': form})
+        else:
+            return render(request, 'new_car.html', {'form': form})
--- djangoCarsProject/urls.py
+++ djangoCarsProject/urls.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from cars.views import new_car_view, ListCarView
+from cars.views import ListCarView, NewCarView
 from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', ListCarView.as_view(), name='cars_list'),
-    path('new_car/', new_car_view, name='new_car'),
+    path('new_car/', NewCarView.as_view(), name='new_car'),
     path('register/', accounts_views.register_view, name='register'),
     path('login/', accounts_views.login_view, name='login'),
     path('logout/', accounts_views.logout_view, name='logout'),
